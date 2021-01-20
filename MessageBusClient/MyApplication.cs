17024c10a607cd4c5d35cbddff66275ef388c803
stauent <stauent@yahoo.ca>
2021-01-20 15:38:43 -0500

    Initial Commit - Dependent projects not added yet

---
+++ MessageBusClient/MyApplication.cs
@@ -0,0 +1,73 @@
+﻿using ConfigurationAssistant;
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.ServiceBus;
+using ServiceBusFacade;
+
+namespace MessageBusListener
+{
+    public class RegisterClientRequest
+    {
+        public string ClientName { get; set; }
+        public string ConsultantName { get; set; }
+    }
+
+    public class MyApplication
+    {
+        private readonly IApplicationRequirements<MyApplication> _requirements;
+        private readonly IMessageBus _messsageBus;
+
+        /// <summary>
+        /// We use constructor dependency injection to the interfaces we need at runtime
+        /// </summary>
+        /// <param name="requirements"></param>
+        public MyApplication(IApplicationRequirements<MyApplication> requirements, IMessageBus messsageBus)
+        {
+            _requirements = requirements;
+            _messsageBus = messsageBus;
+        }
+
+        /// <summary>
+        /// This is the application entry point. 
+        /// </summary>
+        /// <returns></returns>
+        internal async Task Run()
+        {
+            $"Application Started at {DateTime.UtcNow}".TraceInformation();
+
+            await DoWork();
+
+            $"Application Ended at {DateTime.UtcNow}".TraceInformation();
+
+            Console.WriteLine("PRESS <ENTER> TO EXIT");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// All work is done here
+        /// </summary>
+        /// <returns></returns>
+        internal async Task DoWork()
+        {
+            await _messsageBus.Subscribe("RegisterClientSubscriber", MessageHandler, ErrorHandler);
+        }
+
+        static void MessageHandler(IRoutableMessage message)
+        {
+            message.TraceInformation("Received from service bus");
+            RegisterClientRequest request = message.GetPayload<RegisterClientRequest>();
+            request.TraceInformation("Payload");
+        }
+
+        static async Task ErrorHandler(Exception exception)
+        {
+            Console.WriteLine(exception);
+        }
+    }
+}
--- MessageBusClient/Program.cs
+++ MessageBusClient/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using ConfigurationAssistant;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ServiceBusFacade;
 
-namespace MessageBusClient
+namespace MessageBusListener
 {
     class Program
     {
-        static void Main(string[] args)
+        public static ConfigurationResults<MyApplication> configuredApplication = null;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Configure the application. Set up IOC container with services required at runtime.
+            configuredApplication = ConsoleHostBuilderHelper.CreateApp<MyApplication>(args, ConfigureLocalServices);
+            await configuredApplication.myService.Run();
+        }
+
+        /// <summary>
+        /// If the default factory method "ConsoleHostBuilderHelper.CreateApp" does not support all the services
+        /// you need at runtime, then you can add them here. "CreateApp" calls this method before any other services
+        /// are added to the IServiceCollection.
+        /// </summary>
+        /// <param name="hostingContext"></param>
+        /// <param name="services"></param>
+        public static void ConfigureLocalServices(HostBuilderContext hostingContext, IServiceCollection services, IAppConfigSections sections)
+        {
+            // Register the appropriate interfaces depending on the environment
+            bool useKeyVaultKey = !string.IsNullOrEmpty(sections.appIntialConfig.KeyVaultKey);
+            ConfigureServiceBus.Initialize(useKeyVaultKey, services);
         }
     }
 }
