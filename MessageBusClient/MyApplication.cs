afec9c53211a6a65b89b3a0d08130e5b4eaa7eba
stauent <stauent@yahoo.ca>
2021-01-28 11:51:23 -0500

    Minor change

--- MessageBusClient/MyApplication.cs
+++ MessageBusClient/MyApplication.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.ServiceBus;
 using ServiceBusFacade;
 
